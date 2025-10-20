a8a79e26ad557f5f090264e0e17a7dd5570a3e7a
paulapina <pina.131714@e.unavarra.es>
2025-10-20 10:34:29 +0200

    Actualización del proyecto con dependencias

--- myproject/train.py
+++ myproject/train.py
@@ -28,22 +28,6 @@ from pytorch_lightning.loggers import TensorBoardLogger
 
 
 if __name__ == '__main__':
-    """
-    Train and validate the CarBikeClassifier model using PyTorch Lightning.
-    main modified by another colaborator (checking work with github)
-
-    The training uses the CarBikeDataModule data loader and logs metrics via TensorBoard.
-    After training and validation, the trained model weights are saved to the 'models' directory.
-
-    Steps performed:
-    1. Initialize the data module and model.
-    2. Configure TensorBoard logger for metrics tracking.
-    3. Set up a PyTorch Lightning trainer with a max of 3 epochs.
-    4. Train and validate the model.
-    5. Save the trained model weights to disk.
-
-    The model is saved at: '../models/car_bike_model.pth'
-    """
     data_module = CarBikeDataModule()
     model = CarBikeClassifier()
 
